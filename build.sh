3b1ddfa6c251c19d27c83aa2fad888c1df7fea42
Dominik Kilian <Dominik.Kilian@nordicsemi.no>
2020-02-28 12:25:37 +0100

    Added multiple instance support

--- build.sh
+++ build.sh
@@ -93,6 +93,45 @@ elif [ "$1" == "mruSet" ]; then # $2 - list name, $3 - MRU max count, $4 - value
 	echo \"$2\" changed to:
 	echo $VALUE
 
+elif [ "$1" == "instance" ]; then
+
+	case "$2" in  
+	*\ * )
+		printf "Enter a new title: "
+		IFS= read TITLE
+		echo $TITLE > /tmp/-build-vscode-mru-tmp.txt
+		cat $OPT/../opt/instanceTitles >> /tmp/-build-vscode-mru-tmp.txt
+		cat /tmp/-build-vscode-mru-tmp.txt | head -$3 | awk '!seen[$0]++' > $OPT/../opt/instanceTitles
+	;;
+	*)
+		TITLE=$2
+	;;
+	esac
+
+	for NUMBER in 1 2 3 4 5 6 7 8 9 10; do
+		echo Checking instance slot $NUMBER
+		mkdir -p ~/vscode_instances/$NUMBER
+		set +e
+		code -s --user-data-dir /home/doki/vscode_instances/$NUMBER | grep "Please run it again" > /dev/null
+		RESULT=$?
+		set -e
+		if [ "$RESULT" == "0" ]; then
+			echo "Free slot $NUMBER"
+			break
+		fi
+	done
+	echo Starting instance $NUMBER
+	set +e
+	grep "window.title" ~/vscode_instances/$NUMBER/User/settings.json > /dev/null
+	if [ "$?" != "0" ]; then
+		echo "WARNING !!!!"
+		echo "Set window.title settings option and restart instance to correctly show window title!!!"
+		echo "Go to File - Preferences - Settings, search for window.title and add anything after \${dirty}"
+	fi
+	set -e
+	sed -i -E -e "s/(\\\$\{dirty\})([^\\\$]*?)(\\\$\{separator\})?/\1$TITLE\$\{separator\}/g" ~/vscode_instances/$NUMBER/User/settings.json
+	VS_CODE_INSTANCE=$NUMBER code --user-data-dir ~/vscode_instances/$NUMBER
+
 else
 
 	echo "Unknown command"
